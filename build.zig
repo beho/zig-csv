eb9b52125d1c59ac79f5cd049ce8a4ad5bba65ba
beho <sperka@gmail.com>
2022-03-14 21:22:55 +0100

    Target Zig 0.9.1, code formatting

--- build.zig
+++ build.zig
@@ -1,13 +1,25 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
+const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+
     const lib = b.addStaticLibrary("csv", "src/main.zig");
+    lib.setTarget(target);
     lib.setBuildMode(mode);
     lib.install();
 
     var main_tests = b.addTest("test/csv_tokenizer.zig");
     main_tests.addPackagePath("csv", "src/main.zig");
+    main_tests.setTarget(target);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
